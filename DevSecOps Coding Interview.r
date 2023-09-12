acce219a1bc3d45d4d2dfba9e64a57b5ecc1a021
GRΞG <gbg184@gmail.com>
2023-09-11 23:28:31 -0400

    Add files via upload

---
+++ DevSecOps Coding Interview.r
@@ -0,0 +1,48 @@
+numbers = [9, 34, 11, -4, 27, 45]
+
+# find min 
+min_number = min(numbers)
+print(min_number)
+#------------------------------------------------
+numbers = [9, 34, 11, -4, 27, 56]
+
+# find max
+max_number = max(numbers)
+print(max_number)
+#------------------------------------------------
+marks = [65, 34, 68, 34, 4, 60]
+
+# find sum 
+total_marks = sum(marks)
+
+print(total_marks)
+#------------------------------------------------
+marks = [65, 34, 68, 34, 4, 60]
+
+# find median 
+total_marks = sum(marks)/2
+
+print(total_marks)
+#------------------------------------------------
+marks = [65, 34, 68, 34, 4, 60]
+
+# find mean 
+total_marks = sum(marks)/6
+
+print(total_marks)
+#fixed code:
+numbers = [9, 34, 11, -4, 27, 45]
+
+# find min 
+min_number = min(numbers)
+max_number = max(numbers)
+total_marks = sum(numbers)
+total_med = sum(numbers)/2
+total_mean = sum(numbers)/6
+print(min_number)
+print(max_number)
+print(total_marks)
+print(total_med)
+print(total_mean)
+#------------------------------------------------
+#https://www.programiz.com/python-programming/online-compiler/?ref=0c59dff1
